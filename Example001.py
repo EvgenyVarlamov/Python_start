44b5796367d94f671df678144348a06ea5cc0b29
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-07-13 13:39:51 +0300

    Python start, 2 lectures

--- Example001.py
+++ Example001.py
@@ -248,6 +248,13 @@
 # for i in 'qwe-rty':
 #     print(i)
 
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+
 # Строки ###############################################################
 
 # text = 'съешь ещё этих мягких французских булок'
@@ -308,15 +315,157 @@
 # Функции ###############################################################
 # def
 
-def f(x):
-    if x == 1:
-        return 'Целое'
-    elif x == 2.3:
-        return 23
-    else:
-        return
+# def f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
 
 
-arg = 1
-print(f(arg))
-print(type(f(arg)))
+# arg = 1
+# print(f(arg))
+# print(type(f(arg)))
+
+# Работа с файлами #######################################################
+
+# colors = ['red', 'green', 'blue']
+# data=open('file.txt', 'a')
+# data.writelines(colors)  # разделителей не будет
+# data.write('\nLINE 2\n')
+# data.write('LINE 3\n')
+# data.close()               # закрытие файла
+
+#######
+
+# with open('file.txt', 'w') as data:
+#     data.write('line 1\n')
+#     data.write('line 2\n') # закрытие файла автоматически без data.close()
+
+#######
+
+# from importlib.resources import path
+
+# path='file.txt'
+# data = open(path,'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# Функции ###############################################################
+
+# def new_string(symbol, count = 3):
+#     return symbol * count
+# print(new_string('!', 5))   # !!!!!
+# print(new_string('!'))      # !!!
+# print(new_string(4))        # 12
+
+# import Coordinates             # Импорт из файла Coordinates.py
+# print(Coordinates.distance())  # Вызов функции distance()
+
+# import Coordinates as C             # Сокращение название файла Coordinates.py на C
+# print(C.distance())                 # Вызов функции distance()
+
+# def concatenatio(*params):
+#     res: str = ""
+#     for item in params:
+#         res += item
+#     return res
+# print(concatenatio('a', 's', 'd', 'w'))  # asdw
+# print(concatenatio('a', '1', 'd', '2'))  # a1d2
+# # print(concatenatio(1, 2, 3, 4)) # TypeError: ...
+
+# def fib(n):        # Рекурсия Фибоначчи
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+# list = []
+# for e in range(1, 10):
+#     list.append(fib(e))
+# print(list) # 1 1 2 3 5 8 13 21 34
+
+# Кортежи ###############################################################
+
+# t = (28, 9, 1990)
+# print(type(t))
+# a = (3, 4, 5, 6, 7, 8)
+# # print(a[0])
+# # print(a[-1])
+# # print(a[-2])
+# for item in a:
+#     print(item, end=', ')
+
+# t = tuple(['red', 'green', 'blue'])
+# print(t[0])      # red
+# print(t[2])      # blue
+# print(t[-1])     # blue
+# print(t[-2])     # green
+
+# t = tuple(['red', 'green', 'blue'])
+# red, green, blue = t
+# print('r:{} g:{} b:{}'.format(red, green, blue))
+# # r:red g:green b:blue
+
+# Словари ###############################################################
+
+# dictionary = {'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# dictionary = \
+#     {
+#         'up': '↑',
+#         'left': '←',
+#         'down': '↓',
+#         'right': '→'
+#     }
+# print(dictionary)  # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+# print(dictionary['left'])  # ←
+# типы ключей могут отличаться
+
+# for k in dictionary.keys():
+#     print(k)
+
+# for v in dictionary.values():
+#     print(v)
+
+# print(dictionary['up'])   # ↑
+# # типы ключей могут отличаться
+# dictionary['left'] = '⇐'
+# print(dictionary['left'])  # ⇐
+# #print(dictionary['type'])  # KeyError: 'type'
+# del dictionary['left'] # удаление элемента
+# for item in dictionary: # for (k,v) in dictionary.items():
+#     print('{}: {}'.format(item, dictionary[item]))
+
+# Множества #############################################################
+
+# colors = {'red', 'green', 'blue'}
+# print(colors)   # {'red', 'green', 'blue'}
+# colors.add('red')
+# print(colors)   # {'red', 'green', 'blue'}
+# colors.add('gray')
+# print(colors)   # {'red', 'green', 'blue','gray'}
+# colors.remove('red')
+# print(colors)   # {'green', 'blue','gray'}
+# colors.remove('red') # KeyError: 'red'
+# colors.discard('red')  # ok
+# print(colors)   # {'green', 'blue','gray'}
+# colors.clear()   # { }
+# print(colors)   # set()
+
+# a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy()  # c = {1, 2, 3, 5, 8}
+# u = a.union(b)  # u = {1, 2, 3, 5, 8, 13, 21}
+# i = a.intersection(b)  # i = {8, 2, 5}
+# dl = a.difference(b)  # dl = {1, 3}
+# dr = b.difference(a)  # dr = {13, 21}
+
+# q = a \
+#     .union(b) \
+#     .difference(a.intersection(b))
+# # {1, 21, 3, 13}
+
+a = {1, 2, 3, 5, 8}
+b = frozenset(a)
+print(b) # frozenset({1, 2, 3, 5, 8})
